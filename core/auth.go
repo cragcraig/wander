edabe07c68b21249c54d4d81199c26e5fc5c4d5c
Craig Harrison <craigdh@google.com>
2015-02-03 15:23:00 -0700

    negotiate telnet character-by-character buffer mode and handle all line editing

--- core/auth.go
+++ core/auth.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 	"time"
+	"unicode"
 )
 
 func AuthNewUsers(c <-chan Connection, users chan<- ActiveUser) {
@@ -17,15 +18,16 @@ func AuthNewUsers(c <-chan Connection, users chan<- ActiveUser) {
 
 func authConnection(id int, conn Connection, users chan<- ActiveUser) {
 	for {
-		conn.Write <- "By what alias would you like to be known?\n"
-		conn.Prompt <- true
+		conn.Write <- "What is your nom de plume?\n"
 		nick, ok := <-conn.Read
 		if !ok {
 			// Client disconnected.
 			return
 		}
-		nick = strings.TrimSpace(nick)
-		if nick != "" {
+		nick = strings.TrimFunc(nick, func(r rune) bool {
+			return !unicode.IsLetter(r) && !unicode.IsDigit(r) && !unicode.IsPunct(r)
+		})
+		if nick != "" && len(nick) < 16 {
 			conn.Write <- fmt.Sprintf("Welcome to a Science Fiction Universe, %v!\n", nick)
 			users <- ActiveUser{id, nick, conn, time.Now()}
 			return
--- core/connection.go
+++ core/connection.go
@@ -1,20 +1,28 @@
 package core
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"net"
 	"strconv"
+	"sync"
 )
 
-var BUFSIZE int = 512
+const (
+	Bufsize int    = 512
+	Newline string = "\r\n"
+)
 
 type Connection struct {
-	netConn net.Conn
-	Read    <-chan string
-	Write   chan<- string
-	Prompt  chan<- bool
+	netConn  net.Conn
+	Read     <-chan string
+	Write    chan<- string
+	rawWrite chan<- string
+	Prompt   chan<- bool
+
+	buffer     []byte
+	bufferLock sync.Mutex
+	echo       bool
 }
 
 func (conn *Connection) Close() {
@@ -22,50 +30,88 @@ func (conn *Connection) Close() {
 	conn.netConn.Close()
 }
 
-func readLines(c chan<- string, r io.Reader) {
-	defer close(c)
-	var buffer bytes.Buffer
-	b := make([]byte, BUFSIZE)
+func readLines(conn *Connection, r chan<- string, reader io.Reader) {
+	defer close(r)
+	b := make([]byte, Bufsize)
 	for {
-		n, err := r.Read(b)
+		n, err := reader.Read(b)
 		if err != nil {
 			return
 		}
+		conn.bufferLock.Lock()
+		telnet_option_mode := false
 		for i := 0; i < n; i++ {
 			next := b[i]
-            // TODO(craig): Handle backspace
-			if strconv.IsPrint(rune(next)) {
-				buffer.WriteByte(next)
-			} else if buffer.Len() != 0 {
-				c <- buffer.String()
-				buffer.Reset()
+			// TODO: handle esc sequences, aka [*
+			if next&0x80 != 0 {
+				// telnet control character
+				fmt.Printf("0x%x\n", next)
+				telnet_option_mode = true
+			} else if telnet_option_mode {
+				// telnet option
+				fmt.Printf("0x%x\n", next)
+				telnet_option_mode = false
+			} else if strconv.IsPrint(rune(next)) { // TODO: enforce a max buf size
+				// printable character
+				if conn.echo {
+					conn.rawWrite <- string(next)
+				}
+				conn.buffer = append(conn.buffer, next)
+			} else if next == 0x7f && conn.echo {
+				// backspace
+				if len(conn.buffer) > 0 {
+					conn.buffer = conn.buffer[:len(conn.buffer)-1]
+					conn.rawWrite <- "\b \b"
+				}
+			} else if len(conn.buffer) > 0 {
+				// newline
+				r <- string(conn.buffer)
+				if conn.echo {
+					conn.rawWrite <- Newline
+				}
+				conn.buffer = conn.buffer[:0]
 			}
 		}
+		conn.bufferLock.Unlock()
 	}
 }
 
-func writeLines(c <-chan string, w io.Writer) {
-	for s := range c {
-        // TODO(craig): Clear line and reprompt after
-		w.Write([]byte(s))
+func writeRaw(w <-chan string, writer io.Writer) {
+	for s := range w {
+		writer.Write([]byte(s))
 	}
 }
 
-func writePrompts(p <-chan bool, w chan<- string) {
+func writeAndPrompt(conn *Connection, wp <-chan string, w chan<- string) {
+	for s := range wp {
+		w <- "\r\x00" + s
+		conn.bufferLock.Lock()
+		buf := string(conn.buffer)
+		conn.bufferLock.Unlock()
+		w <- "\r\x00> " + buf
+	}
+}
+
+func triggerPrompts(p <-chan bool, wp chan<- string) {
 	for _ = range p {
-        // TODO(craig): Print contents of buffer (if typing was interrupted)
-		w <- "> "
+		wp <- ""
 	}
 }
 
 func detachConnection(conn net.Conn) Connection {
-	r := make(chan string)
-	go readLines(r, conn)
-	w := make(chan string)
-	go writeLines(w, conn)
-	p := make(chan bool)
-	go writePrompts(p, w)
-	return Connection{conn, r, w, p}
+	r := make(chan string)  // read
+	w := make(chan string)  // raw write
+	p := make(chan bool)    // trigger prompt
+	wp := make(chan string) // write with prompt
+	connection := Connection{conn, r, wp, w, p, make([]byte, 2048), sync.Mutex{}, true}
+	go readLines(&connection, r, conn)
+	go writeRaw(w, conn)
+	go writeAndPrompt(&connection, wp, w)
+	go triggerPrompts(p, wp)
+	// Set telnet to ECHO (no local buffering)
+	// TODO(craigdh): Check for ACK (\xff\xfd\x03 \xff\xfd\x01) and set echo appropriately.
+	w <- "\xff\xfb\x03\xff\xfb\x01"
+	return connection
 }
 
 func acceptConnectionsForever(ln net.Listener, c chan<- Connection) {
